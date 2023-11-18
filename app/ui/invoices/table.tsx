0cbb9b4e95c94497ddf4f895d7ae37dfee23a0a0
Brian <subutian@gmail.com>
2023-11-18 14:32:41 +1100

    update

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,7 +27,7 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
-                        alt="as"
+                        alt={`${invoice.name}'s profile picture`}
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
